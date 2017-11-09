18d3d05499bc009c0729804c0cdbd18decb0b3ea
zhuharev <kirill@zhuharev.ru>
2017-11-09 08:30:32 +0300

    add deps

---
+++ vendor/github.com/zhuharev/qiwi/history.go
@@ -0,0 +1,156 @@
+// Copyright 2017 Kirill Zhuharev. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package qiwi
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/url"
+	"time"
+)
+
+// History for payment-history endpoints
+type History struct {
+	client *Client
+}
+
+// NewHistory returns new History obj
+func NewHistory(c *Client) *History {
+	return &History{client: c}
+}
+
+// Payments call api and get payments history
+func (h *History) Payments(rows uint, params ...url.Values) (hr *PaymentsResponse, err error) {
+	param := url.Values{}
+
+	{
+		if len(params) > 0 {
+			param = params[0]
+		}
+		param["rows"] = []string{fmt.Sprint(rows)}
+	}
+
+	body, err := h.client.makeRequest(EndpointPaymentsHistory, param)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+
+	bts, err := ioutil.ReadAll(body)
+	if err != nil {
+		return
+	}
+
+	buf := bytes.NewReader(bts)
+
+	log.Printf("%s", bts)
+
+	hr = new(PaymentsResponse)
+
+	dec := json.NewDecoder(buf)
+	err = dec.Decode(hr)
+	return
+}
+
+// PaymentsResponse api response format
+type PaymentsResponse struct {
+	Data        []Txn       `json:"data"`
+	NextTxnID   interface{} `json:"nextTxnId"`
+	NextTxnDate interface{} `json:"nextTxnDate"`
+}
+
+// Txn represent qiwi transaction
+type Txn struct {
+	TxnID      int64     `json:"txnId"`
+	PersonID   int64     `json:"personId"`
+	Date       time.Time `json:"date"`
+	ErrorCode  int       `json:"errorCode"`
+	Error      string    `json:"error"`
+	Status     string    `json:"status"`
+	Type       string    `json:"type"`
+	StatusText string    `json:"statusText"`
+	TrmTxnID   string    `json:"trmTxnId"`
+	Account    string    `json:"account"`
+	Sum        struct {
+		Amount   float64 `json:"amount"`
+		Currency int     `json:"currency"`
+	} `json:"sum"`
+	Commission struct {
+		Amount   float64 `json:"amount"`
+		Currency int     `json:"currency"`
+	} `json:"commission"`
+	Total struct {
+		Amount   float64 `json:"amount"`
+		Currency int     `json:"currency"`
+	} `json:"total"`
+	Provider struct {
+		ID          int           `json:"id"`
+		ShortName   string        `json:"shortName"`
+		LongName    string        `json:"longName"`
+		LogoURL     string        `json:"logoUrl"`
+		Description string        `json:"description"`
+		Keys        string        `json:"keys"`
+		SiteURL     string        `json:"siteUrl"`
+		Extras      []interface{} `json:"extras"`
+	} `json:"provider"`
+	Source                 interface{}   `json:"source"`
+	Comment                string        `json:"comment"`
+	CurrencyRate           int           `json:"currencyRate"`
+	Extras                 []interface{} `json:"extras"`
+	ChequeReady            bool          `json:"chequeReady"`
+	BankDocumentAvailable  bool          `json:"bankDocumentAvailable"`
+	BankDocumentReady      bool          `json:"bankDocumentReady"`
+	RepeatPaymentEnabled   bool          `json:"repeatPaymentEnabled"`
+	FavoritePaymentEnabled bool          `json:"favoritePaymentEnabled"`
+	RegularPaymentEnabled  bool          `json:"regularPaymentEnabled"`
+}
+
+// StatResponse response of stat endpoind
+type StatResponse struct {
+	IncomingTotal []struct {
+		Amount   float64 `json:"amount"`
+		Currency int     `json:"currency"`
+	} `json:"incomingTotal"`
+	OutgoingTotal []struct {
+		Amount   float64 `json:"amount"`
+		Currency int     `json:"currency"`
+	} `json:"outgoingTotal"`
+}
+
+// Stat get sum of incoming and outgoing payments
+func (h *History) Stat(startDate, endDate time.Time, params ...url.Values) (res StatResponse, err error) {
+	param := url.Values{}
+
+	{
+		if len(params) > 0 {
+			param = params[0]
+		}
+		param["startDate"] = []string{startDate.Format(time.RFC3339)}
+		param["endDate"] = []string{endDate.Format(time.RFC3339)}
+	}
+
+	body, err := h.client.makeRequest(EndpointStat, param)
+	if err != nil {
+		return
+	}
+	defer body.Close()
+
+	bts, err := ioutil.ReadAll(body)
+	if err != nil {
+		return
+	}
+
+	buf := bytes.NewReader(bts)
+
+	log.Printf("%s", bts)
+
+	dec := json.NewDecoder(buf)
+	err = dec.Decode(&res)
+
+	return
+}
